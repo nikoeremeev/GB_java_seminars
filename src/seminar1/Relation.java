bf045e7d7268df61bb80ac5389b3f783370e9dd6
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-08-10 20:15:03 +0300

    Добавил именнованные перечисления для указания родства (родственную связь)

---
+++ src/seminar1/Relation.java
@@ -0,0 +1,18 @@
+package seminar1;
+
+public enum Relation {
+    FATHER,
+    MOTHER,
+    SON,
+    DAUGHTER,
+    BROTHER,
+    SISTER,
+    UNCLE,
+    AUNT,
+    GRANDMA,
+    GRANDPA,
+    NEPHEW,
+    NIECE,
+    HUSBUND,
+    WIFE
+}
