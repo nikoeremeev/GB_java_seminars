b65bf343c40e0a9d7f698d7ea3c5e10244df3892
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-08-28 19:48:45 +0300

    Создал класс для добавления задач в список, сортировку по сроку выполнения и приоритету

---
+++ src/seminar4_homework/TaskList.java
@@ -0,0 +1,56 @@
+package seminar4_homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private final static List<Task> tasks = new ArrayList<>();
+
+    public static List<Task> getTasks() {
+        return tasks;
+    }
+
+    public static void add(Task task) {
+        tasks.add(task);
+    }
+
+    public static Integer getTaskById(int id) {
+        int i = 0;
+        boolean notFind = true;
+        while (notFind && i < tasks.size()) {
+            if (tasks.get(i).getId() == id) {
+                notFind = false;
+            } else {
+                i++;
+            }
+        }
+        if (notFind) {
+            i = -1;
+        }
+        return i;
+    }
+
+    public static void removeById(int id) {
+        if (getTaskById(id) > -1) {
+            int index = getTaskById(id);
+            tasks.remove(index);
+        }
+    }
+
+    public static void updateByDeadline() {
+        tasks.sort(new DeadlineComparator());
+    }
+
+    public static void updateByPriority() {
+        tasks.sort(new PriorityComparator());
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (Task element : tasks) {
+            str.append(element + "\n");
+        }
+        return str.toString();
+
+    }
+}
