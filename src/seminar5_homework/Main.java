9d991f02b9072b9338685234a565772540781a6c
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-09-07 20:22:57 +0300

    Печать списка пользователей

--- src/seminar5_homework/Main.java
+++ src/seminar5_homework/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("--------------------------------------------------------------\n");
         UserCategory h = new UserCategory("nick",20,"as@mail.ru",UserType.USER);
+        UserCategory h1 = new UserCategory("John",10,"ad@mail.ru",UserType.ADMIN);
+        UserCategory h2 = new UserCategory("Boris",20,"da@mail.ru",UserType.GUEST);
+        UserCategory h3 = new UserCategory("Ivan",30,"sf@mail.ru",UserType.MODERATOR);
         System.out.print(h);
         System.out.print("--------------------------------------------------------------\n");
         UserMessage m = new UserMessage(1, new Date(),h,"TEXT");
@@ -14,6 +17,10 @@ public class Main {
         System.out.print("--------------------------------------------------------------\n");
         UsersList usersList = new UsersList();
         usersList.addUser(h);
+        usersList.addUser(h1);
+        usersList.addUser(h2);
+        usersList.addUser(h3);
+        usersList.printUsersList();
         System.out.print("--------------------------------------------------------------\n");
     }
 
--- src/seminar5_homework/UsersList.java
+++ src/seminar5_homework/UsersList.java
@@ -1,12 +1,32 @@
 package seminar5_homework;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UsersList {
-    private ArrayList<UserCategory> userList= new ArrayList<>();
+    private static List<UserCategory> userList= new ArrayList<>();
 
     public void addUser(UserCategory user){
         userList.add(user);
         System.out.println("User "+user.getNickName()+" successfully added;\n");
+    }
+
+    public static UserCategory getUser(String userNickname) {
+        UserCategory temp = new UserCategory("NULL",0,"noEmail", UserType.USER);
+        for (UserCategory item : userList) {
+            if (item.getNickName().equals(userNickname)) {
+                temp = item;
+            }
+        }
+
+        return temp;
+
+    }
+    public void printUsersList() {
+        System.out.println("User NickName" +"  |  "+ "User age" +"  |  "+ "User e-mail" +"  |  "+  "User Role");
+        for (UserCategory item : userList) {
+            System.out.println("        "+item.getNickName()+"   |   "+item.getUserAge()+"   |   "+item.getEmailAddress()+"   |   "+item.getUserType()+"\n");
+
+        }
     }
 }
